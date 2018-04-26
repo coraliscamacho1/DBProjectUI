0bb2d96b80a45de286810c8fcb42607d1458eb6e
coraliscamacho1 <coralis.camacho1@upr.edu>
2018-04-26 11:32:55 -0400

    First commit

---
+++ appjs/chat.js
@@ -0,0 +1,59 @@
+angular.module('AppChat').controller('ChatController', ['$http', '$log', '$scope',
+    function($http, $log, $scope) {
+        var thisCtrl = this;
+
+        this.messageList = [];
+        this.counter  = 2;
+        this.newText = "";
+
+        this.loadMessages = function(){
+            // Get the messages from the server through the rest api
+            //thisCtrl.messageList.push({"id": 1, "text": "Hola Mi Amigo", "author" : "Bob",
+            //"like" : 4, "nolike" : 1});
+            //thisCtrl.messageList.push({"id": 2, "text": "Hello World", "author": "Joe",
+            //    "like" : 11, "nolike" : 12});
+
+            var url = "http://localhost:5000/SikitrakeChat/Messages/GroupChats/1";
+            $http.get(url).then(
+                function (response){
+                    console.log("response: " + JSON.stringify(response));
+                    thisCtrl.messageList = response.data.Messages;
+                    $log.error("Message Loaded: ", JSON.stringify(thisCtrl.messageList));
+            }, // error callback
+            function (response){
+                // This is the error function
+                // If we get here, some error occurred.
+                // Verify which was the cause and show an alert.
+                var status = response.status;
+                if (status == 0){
+                    alert("No hay conexion a Internet");
+                }
+                else if (status == 401){
+                    alert("Su sesion expiro. Conectese de nuevo.");
+                }
+                else if (status == 403){
+                    alert("No esta autorizado a usar el sistema.");
+                }
+                else if (status == 404){
+                    alert("No se encontro la informacion solicitada.");
+                }
+                else {
+                    alert("Error interno del sistema.");
+                }
+            });
+
+            $log.error("Message Loaded: ", JSON.stringify(thisCtrl.messageList));
+        };
+
+        this.postMsg = function(){
+            var msg = thisCtrl.newText;
+            // Need to figure out who I am
+            var author = "Me";
+            var nextId = thisCtrl.counter++;
+            thisCtrl.messageList.unshift({"id": nextId, "text" : msg, "author" : author, "like" : 0, "nolike" : 0});
+            thisCtrl.newText = "";
+        };
+
+        this.loadMessages();
+        console.log("Juaan");
+}]);
